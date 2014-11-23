20dfb23f2ad425c644ecd46a55ea7013532d7c9c
unknown <sxt5244@GDD-8.main.ad.rit.edu>
2014-11-23 18:53:05 -0500

    Wall script

---
+++ Assets/Scripts/Game/Wall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Utils;
+
+public class Wall : GameBehaviour {
+
+	// Use this for initialization
+    [EnumMask]
+    public Dimensions ActiveDimensions;
+
+    private GameObject meshContainer;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        meshContainer = this.gameObject;
+    }
+
+    public override void ShiftTo(Dimensions dimension)
+    {
+        base.ShiftTo(dimension);
+
+        meshContainer.SetActive((dimension & ActiveDimensions) == dimension);
+    }
+}
