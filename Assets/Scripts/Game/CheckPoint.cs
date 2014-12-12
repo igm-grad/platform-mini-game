983479f92cfdb556b68cef081838d3babb926b57
zach whitman <zdw1791@rit.edu>
2014-12-12 16:14:38 -0500

    srini fixed the checkpoints, after playtesting, this might be too hard for a level 2

--- Assets/Scripts/Game/CheckPoint.cs
+++ Assets/Scripts/Game/CheckPoint.cs
@@ -6,7 +6,7 @@ public class CheckPoint : GameBehaviour {
 	void OnTriggerEnter2D(Collider2D coll)
 	{
 		Debug.Log (coll.gameObject.name);
-		if (coll.gameObject.name == "Mesh") 
+		if (coll.gameObject.name == "Body") 
 		{
 			World.SetCheckpoint();		
 			Debug.Log("checkpoint set");
