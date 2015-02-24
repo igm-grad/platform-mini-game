c000aca40dd7cddf4de488e4fe52a72ce462ad19
Henrique Chaltein <hxs1151@rit.edu>
2015-02-24 16:50:39 -0500

    Level Selector

---
+++ Assets/Scripts/Game/GoToLevel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToLevel : MonoBehaviour
+{
+	public int LevelNumber;
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.name == "Player") 
+		{
+			Application.LoadLevel(LevelNumber);
+			World.ResetWorld();
+		}
+	}
+}
